67da2dc815e7c77f4fd357d0b5bda208077c08a5
Rohit Patra <rpatra332@gmail.com>
2024-02-19 23:15:45 +0530

    Added the **JWTRequestFilter** before AuthorizationFilter to run jwtFilter before the AuthorizationFilter
    Made changes to the SecurityFilterChain so that endpoint that does need authentication are supposed to be excluded, from authentication.
    
    Affected endpoints:
    - **/auth/register**
    - **/auth/login**
    - **/products**

--- src/main/java/com/rp/ecommercebackend/api/security/WebSecurityConfig.java
+++ src/main/java/com/rp/ecommercebackend/api/security/WebSecurityConfig.java
@@ -1,18 +1,28 @@
 package com.rp.ecommercebackend.api.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
+@AllArgsConstructor
 public class WebSecurityConfig {
+
+    private JWTRequestFilter jwtRequestFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSec) throws Exception {
         httpSec.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/product", "/auth/*")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
         return httpSec.build();
     }
 }
